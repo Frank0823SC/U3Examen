963e275efd489451a6cf5dad8a215f39ea6a1a8c
Frank0823SC <71744464+Frank0823SC@users.noreply.github.com>
2020-12-02 16:31:38 -0600

    Create Nodo2.java

---
+++ Exa/Nodo2.java
@@ -0,0 +1,12 @@
+package Exa;
+public class Nodo2 {
+    String elemento;
+    int indice; 
+	Nodo2 sig;
+	
+	public Nodo2(String elemento, int indice) {
+		this.elemento=elemento;
+		this.indice=indice;
+	    sig=null;
+    }
+}
